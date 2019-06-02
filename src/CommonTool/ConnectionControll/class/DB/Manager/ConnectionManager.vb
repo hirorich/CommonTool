6c157b5e402e26af4456860161de4f5cb73240b0
hirorich <ch.dim7ir@gmail.com>
2019-06-02 17:06:59 +0900

    接続情報管理クラス作成

--- src/CommonTool/ConnectionControll/class/DB/Manager/ConnectionManager.vb
+++ src/CommonTool/ConnectionControll/class/DB/Manager/ConnectionManager.vb
@@ -1,28 +1,110 @@
-﻿Namespace DB
+﻿Imports LogUtil.LOG
 
+Namespace DB
+
+    ''' <summary>
+    ''' 接続情報管理クラス
+    ''' </summary>
     Public MustInherit Class ConnectionManager
         Implements IDisposable
 
+        ''' <summary>
+        ''' 接続情報一覧
+        ''' </summary>
         Private dbcontrollers As Dictionary(Of String, ConnectionController)
 
         Protected Friend Sub New()
-            dbcontrollers = New Dictionary(Of String, ConnectionController)
+            Me.dbcontrollers = New Dictionary(Of String, ConnectionController)
         End Sub
 
-        Protected Function Add(ByVal dbname As String, ByVal connection As IDbConnection) As Boolean
+        ''' <summary>
+        ''' 接続情報を追加
+        ''' </summary>
+        ''' <param name="dbname">接続先</param>
+        ''' <param name="controller">レコード操作クラス</param>
+        ''' <returns>True:成功 / False:失敗</returns>
+        Protected Function Add(ByVal dbname As String, ByVal controller As ConnectionController) As Boolean
+            Try
 
+                ' 接続情報追加
+                Call Me.dbcontrollers.Add(dbname, controller)
+                Add = True
+
+            Catch ex As Exception
+                Call LogTool.WriteLog(New Exception("接続情報の追加に失敗しました。", ex))
+                Add = False
+            End Try
         End Function
 
+        ''' <summary>
+        ''' 接続情報を削除
+        ''' </summary>
+        ''' <param name="dbname">接続先</param>
+        ''' <returns>True:成功 / False:失敗</returns>
         Public Function Remove(ByVal dbname As String) As Boolean
+            Dim controller As ConnectionController
 
+            ' 存在しない場合
+            If Not Me.Exists(dbname) Then
+                LogTool.WriteLog("指定した接続先：" & dbname & " が追加されていません。")
+                Return False
+            End If
+
+            Try
+
+                ' 接続情報解放
+                controller = Me.CreateDbController(dbname)
+                Call controller.Dispose()
+                controller = Nothing
+
+                ' 接続情報削除
+                Remove = Me.dbcontrollers.Remove(dbname)
+
+            Catch ex As Exception
+                Call LogTool.WriteLog(New Exception("接続情報の削除に失敗しました。", ex))
+                Remove = False
+            End Try
         End Function
 
+        ''' <summary>
+        ''' 接続情報の存在チェック
+        ''' </summary>
+        ''' <param name="dbname">接続先</param>
+        ''' <returns>True:存在する / False:存在しない</returns>
         Public Function Exists(ByVal dbname As String) As Boolean
+            Try
 
+                ' 接続先存在確認
+                Exists = Me.dbcontrollers.ContainsKey(dbname)
+
+            Catch ex As Exception
+                Call LogTool.WriteLog(New Exception("接続情報の存在チェックに失敗しました。", ex))
+                Exists = False
+            End Try
         End Function
 
+        ''' <summary>
+        ''' 接続先を基にレコード操作クラスを取得
+        ''' </summary>
+        ''' <param name="dbname">接続先</param>
+        ''' <returns>レコード操作クラス</returns>
         Protected Function CreateDbController(ByVal dbname As String) As ConnectionController
 
+            ' 存在しない場合
+            If Not Me.Exists(dbname) Then
+                LogTool.WriteLog("指定した接続先：" & dbname & " が追加されていません。")
+                Return Nothing
+            End If
+
+            Try
+
+                ' 接続情報取得
+                CreateDbController = Me.dbcontrollers.Item(dbname)
+
+            Catch ex As Exception
+                Call LogTool.WriteLog(New Exception("接続情報の取得に失敗しました。", ex))
+                CreateDbController = Nothing
+            End Try
         End Function
 
 #Region "IDisposable Support"
