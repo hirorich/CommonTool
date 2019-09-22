08a07e1489046b0ddfc7bfe7bff917832a5d0e9a
hirorich <ch.dim7ir@gmail.com>
2019-09-22 15:19:28 +0900

    セクション、キーに空白を認めないよう対応

--- src/CommonTool/CommonTool.FileIO/IniControl.vb
+++ src/CommonTool/CommonTool.FileIO/IniControl.vb
@@ -4,6 +4,7 @@ Imports CommonTool.FileIO.WinAPI
 ''' <summary>
 ''' iniファイル読み書きクラス
 ''' </summary>
+''' <remarks>空文字のセクション、キーは無視する</remarks>
 Public Class IniControl
 
     ''' <summary>
@@ -16,13 +17,14 @@ Public Class IniControl
     ''' </summary>
     ''' <param name="filename">読み書き対象ファイル</param>
     Public Sub New(ByVal filename As String)
-        Me.m_filename = filename
+        Me.m_filename = filename.Trim
     End Sub
 
     ''' <summary>
     ''' セクション一覧読み込み
     ''' </summary>
-    ''' <returns></returns>
+    ''' <returns>セクション一覧</returns>
+    ''' <remarks>空文字のセクションは取得しない</remarks>
     Public Function ReadSections() As String()
         Try
 
@@ -35,7 +37,24 @@ Public Class IniControl
                 Throw New Exception()
             End If
 
-            ReadSections = buffer.Split(Constants.vbNullChar)
+            ' 空白以外を取得
+            Dim objSectionList As List(Of String) = New List(Of String)
+            For Each sec As String In buffer.Trim.Split(Constants.vbNullChar)
+
+                ' セクションが空白の場合飛ばす
+                If String.IsNullOrWhiteSpace(sec) Then
+                    Continue For
+                End If
+
+                objSectionList.Add(sec.Trim)
+            Next
+
+            ' 取得数が0の場合は例外をスロー
+            If objSectionList.Count = 0 Then
+                Throw New Exception()
+            End If
+
+            ReadSections = objSectionList.ToArray
 
         Catch ex As Exception
             Throw
@@ -46,20 +65,50 @@ Public Class IniControl
     ''' キー一覧読み込み
     ''' </summary>
     ''' <param name="section">セクション</param>
-    ''' <returns></returns>
+    ''' <returns>キー一覧</returns>
+    ''' <remarks>空文字のセクション、キーは取得しない</remarks>
     Public Function ReadKeys(ByVal section As String) As String()
         Try
 
+            ' セクションの入力チェック
+            If String.IsNullOrWhiteSpace(section) Then
+                Throw New Exception()
+            End If
+
             ' 値読み込み
             Dim buffer As String = Space(1024)
-            Dim intRet As Integer = Kernel32.GetPrivateProfileSection(section, buffer, buffer.Length - 1, Me.m_filename)
+            Dim intRet As Integer = Kernel32.GetPrivateProfileSection(section.Trim, buffer, buffer.Length - 1, Me.m_filename)
 
             ' 失敗時は例外をスロー
             If intRet <= 0 Then
                 Throw New Exception()
             End If
 
-            ReadKeys = buffer.Split(Constants.vbNullChar)
+            ' 空白以外からキーを取得
+            Dim objKeyList As List(Of String) = New List(Of String)
+            Dim key As String = String.Empty
+            For Each keyvalue As String In buffer.Trim.Split(Constants.vbNullChar)
+
+                ' キーと値のセットが空白の場合飛ばす
+                If String.IsNullOrWhiteSpace(keyvalue) Then
+                    Continue For
+                End If
+
+                ' キーが空白の場合飛ばす
+                key = keyvalue.Trim.Split("=")(0)
+                If String.IsNullOrWhiteSpace(key) Then
+                    Continue For
+                End If
+
+                objKeyList.Add(key.Trim)
+            Next
+
+            ' 取得数が0の場合は例外をスロー
+            If objKeyList.Count = 0 Then
+                Throw New Exception()
+            End If
+
+            ReadKeys = objKeyList.ToArray
 
         Catch ex As Exception
             Throw
@@ -71,20 +120,29 @@ Public Class IniControl
     ''' </summary>
     ''' <param name="section">セクション</param>
     ''' <param name="key">キー</param>
-    ''' <returns></returns>
+    ''' <returns>値</returns>
+    ''' <remarks>空文字のセクション、キーは取得しない</remarks>
     Public Function ReadValue(ByVal section As String, ByVal key As String) As String
         Try
 
+            ' セクション、キーの入力チェック
+            If String.IsNullOrWhiteSpace(section) Then
+                Throw New Exception()
+            End If
+            If String.IsNullOrWhiteSpace(key) Then
+                Throw New Exception()
+            End If
+
             ' 値読み込み
             Dim objBuilder As StringBuilder = New StringBuilder(1024)
-            Dim intRet As Integer = Kernel32.GetPrivateProfileString(section, key, String.Empty, objBuilder, objBuilder.Capacity - 1, Me.m_filename)
+            Dim intRet As Integer = Kernel32.GetPrivateProfileString(section.Trim, key.Trim, String.Empty, objBuilder, objBuilder.Capacity - 1, Me.m_filename)
 
             ' 失敗時は例外をスロー
             If intRet <= 0 Then
                 Throw New Exception()
             End If
 
-            ReadValue = objBuilder.ToString()
+            ReadValue = objBuilder.ToString().Trim
         Catch ex As Exception
             Throw
         End Try
@@ -96,10 +154,23 @@ Public Class IniControl
     ''' <param name="section">セクション</param>
     ''' <param name="key">キー</param>
     ''' <param name="value">値</param>
+    ''' <remarks>空文字のセクション、キーは書き込まない</remarks>
     Public Sub WriteValue(ByVal section As String, ByVal key As String, ByVal value As String)
         Try
+
+            ' セクション、キー、値の入力チェック
+            If String.IsNullOrWhiteSpace(section) Then
+                Throw New Exception()
+            End If
+            If String.IsNullOrWhiteSpace(key) Then
+                Throw New Exception()
+            End If
+            If value Is Nothing Then
+                Throw New Exception()
+            End If
+
             ' 値書き込み
-            Dim intRet As Integer = Kernel32.WritePrivateProfileString(section, key, value, Me.m_filename)
+            Dim intRet As Integer = Kernel32.WritePrivateProfileString(section.Trim, key.Trim, value.Trim, Me.m_filename)
 
             ' 失敗時は例外をスロー
             If intRet <= 0 Then
